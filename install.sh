8bafd32f0ea700143407159acd8cc9dc41387b7d
evan2m <evan2m@gmail.com>
2012-03-19 16:20:34 -0700

    Fix install script.

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-git clone git://github.com/evan2m/dotfiles  $HOME/dotfiles
-cd dotfiles
 mkdir -p "$HOME/lib"
 ln -si "$PWD/zsh/oh-my-zsh" "$HOME/lib/oh-my-zsh"
 ln -si "$PWD/zsh/t" "$HOME/lib/t"
@@ -11,6 +9,7 @@ ln -si "$PWD/vim/vimrc" "$HOME/.vimrc"
 ln -si "$PWD/vim" "$HOME/.vim"
 ln -si "$PWD/git/gitignore" "$HOME/.gitignore"
 ln -si "$PWD/git/gitconfig" "$HOME/.gitconfig"
+ln -si "$PWD/tmux/tmux.conf" "$HOME/.tmux.conf"
 mkdir -p "$HOME/.vim/tmp/backup"
 mkdir -p "$HOME/.vim/tmp/undo"
 mkdir -p "$HOME/.vim/tmp/swap"
